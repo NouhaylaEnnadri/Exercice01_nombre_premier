0a7c8dab564529d89fe78d45818dff1481621b4a
lenovo <lenovo@DESKTOP-H5O83VN>
2022-10-25 12:25:51 +0200

    Exercice01

--- Exec/Program.cs
+++ Exec/Program.cs
@@ -4,12 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exec
+namespace github
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int num1;
+            do
+            {
+                Console.WriteLine("Entrer un nombre:");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+            } while (num1 <= 0);
+
+            if (IsPrime(num1)) { Console.WriteLine("est premier"); }
+            else
+            {
+                Console.WriteLine("Pas Premier");
+
+            }
+            Console.ReadLine();
+
+        }
+        public static bool IsPrime(int num)
+        {
+
+            bool isPrime = true;
+
+            for (int i = 1; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+
+                }
+
+            }
+            return isPrime;
         }
     }
 }
+
